e1d7a0e297046c14f9d4243c1452245db1f772c8
unknown <joseph.harley02@gmail.com>
2024-03-12 15:02:24 -0400

    scraper saves posts to db and created route to return posts by user

--- handler_user.go
+++ handler_user.go
@@ -40,3 +40,16 @@ func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, req *http.Request
 func (cfg *apiConfig) handlerGetUserByApiKey(w http.ResponseWriter, req *http.Request, user database.User) {
 	respondWithJSON(w, http.StatusOK, databaseUserToUser(user))
 }
+
+func (cfg *apiConfig) handlerGetPostsByUser(w http.ResponseWriter, req *http.Request, user database.User) {
+	posts, err := cfg.DB.GetPostsByUser(req.Context(), database.GetPostsByUserParams{
+		UserID: user.ID,
+		Limit:  10,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("Couldn't get posts %v", err))
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, databasePostsToPosts(posts))
+}
